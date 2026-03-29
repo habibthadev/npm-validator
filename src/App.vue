c9a6bc4a8e0e9d67a58acf8cb830e2d1448b00f6
habibthadev <adebayohabib7@gmail.com>
2026-03-29 13:12:38 +0100

    Add root App component with header, footer, and router view

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import ErrorBoundary from '@/components/ErrorBoundary.vue';
+import AppHeader from '@/components/layout/AppHeader.vue';
+import AppFooter from '@/components/layout/AppFooter.vue';
+</script>
+
+<template>
+  <ErrorBoundary>
+    <AppHeader />
+    <RouterView />
+    <AppFooter />
+  </ErrorBoundary>
+</template>
